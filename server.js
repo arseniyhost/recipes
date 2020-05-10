77b7c755fe721efbb8bb4274cce706041a80177d
arseniyhost <46376590+arseniyhost@users.noreply.github.com>
2020-05-10 11:22:46 +0300

    herokubuild

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ app.use('/api/auth', require('./routes/api/auth'));
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/my-app/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/my-app', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
